185577914db15a52095b68de102e451119057e51
Markodj99 <makimarkodjurdjevic@gmail.com>
2023-07-26 15:22:23 +0200

    1

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,27 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db) => _db = db;
+
+        public IActionResult Index() => View(_db.Categories.ToList());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Category c)
+        {
+            if(c.Name == c.DisplayOrder.ToString()) ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the name.");
+            if (!ModelState.IsValid) return View();
+            _db.Categories.Add(c);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
